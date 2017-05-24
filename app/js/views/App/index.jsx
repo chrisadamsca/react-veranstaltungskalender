ed5248bf527609bdf598794eced2e24ca71ba804
Work <work@Antonys-MacBook-Pro.local>
2017-05-24 12:11:52 +0200

    Dist Ordner untracked & Material Design Package installiert

--- app/js/views/App/index.jsx
+++ app/js/views/App/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from '../../components/Global/AppBar';
 import NavBar from '../../components/Global/NavBar';
 
@@ -7,11 +8,13 @@ export default class App extends Component {
     const { children } = this.props;
 
     return (
-      <div className='App'>
-        <AppBar />
-        { children }
-        <NavBar />
-      </div>
+      <MuiThemeProvider>
+        <div className='App'>
+          <AppBar />
+          { children }
+          <NavBar />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
